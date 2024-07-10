889ff49e7d5551ba252bbc798423b7713f3fc849
codephilip <philip@meiers.in>
2024-07-10 10:29:06 -0600

    fix: model

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: {
-    type: String,
+    type: String, //TODO: Add validation for username
     unique: true, // Make the username field unique
   },
   password: String,
